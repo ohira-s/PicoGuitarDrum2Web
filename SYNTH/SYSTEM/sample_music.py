dc8e8a51fda60db64091b0786b5dcd45de0c3490
Shunsuke Ohira <sohira@thymes-square.net>
2025-04-10 15:46:17 +0900

    init

---
+++ SYNTH/SYSTEM/sample_music.py
@@ -0,0 +1,12 @@
+{
+    "NAME": "<< MUSIC TITLE is HERE >>",
+    "MUSIC":[
+        ["<< TONE >>",  "<< CHORD >>",  "<< LOW | HIGH >>",  "<< BASE TONE >>",  << SCALE >>, "<< LYRICS >>",
+                                                                                              "<< PLAY TIMINGS >>"],
+            :
+        ["C",  "M",  "LOW",  "G", 4, "Music lyrics",
+                                     "*     *"]
+        ["C",  "m7", "LOW",  "",  4, "is here."
+                                     "*  *"]
+    ]
+}
